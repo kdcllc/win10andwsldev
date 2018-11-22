0e26ccb9eae43c4d52beaa6fb24ec144ba3be2d6
kdcllc <info@kingdavidconsulting.com>
2018-11-21 23:00:09 -0500

    update script

--- wsl-dev-tools.md
+++ wsl-dev-tools.md
@@ -13,8 +13,26 @@
 4. Install [nvm](https://github.com/creationix/nvm)
 
 ```bash
-  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | sudo bash
 ```
+
+5. Install Node.js
+
+```bash
+    export NODE_VERSION=10.13.0 && \
+    nvm install ${NODE_VERSION} && \
+    nvm use ${NODE_VERSION}
+```
+
+6. [Install Docker Daemon and Docker Compose](./wsl_docker_setup.sh)
+
+```bash
+    curl -o- https://raw.githubusercontent.com/kdcllc/wind10wsldev/master/wsl_docker_setup.sh | bash 
+```
+
+ - Or follow the steps in this article [Setting Up Docker for Windows and WSL to Work Flawlessly](https://nickjanetakis.com/blog/setting-up-docker-for-windows-and-wsl-to-work-flawlessly)
+
+7. Kubernetes
 
 
 ## Clean up WSL
--- wsl_docker_setup.sh
+++ wsl_docker_setup.sh
@@ -1,16 +1,16 @@
 # Install Docker Daemon/Client
 
-ENV DOCKER_VERSION docker-18.09.0.tgz
-ENV DOCKER_COMPOSE_VERSION 1.23.1
+export DOCKER_VERSION=docker-18.09.0.tgz
+export DOCKER_COMPOSE_VERSION=1.23.1
 
-curl https://download.docker.com/linux/static/stable/x86_64/docker-18.09.0.tgz > ~/docker.tar.gz
+curl https://download.docker.com/linux/static/stable/x86_64/${DOCKER_VERSION} > ~/docker.tar.gz
 tar xzvf ~/docker.tar.gz
 sudo mv ~/docker/docker /usr/local/bin/docker
 rm -rf ~/docker
 rm -f ~/docker.tar.gz
 
 # Install docker-compose client
-sudo curl -L https://github.com/docker/compose/releases/download/1.23.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose && \
+sudo curl -L https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose && \
 sudo chmod +x /usr/local/bin/docker-compose
 
 # point client to docker engine running in windows
